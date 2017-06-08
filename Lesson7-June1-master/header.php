1fd2ce72559abc5b5e84b92f92a9129a50fb52ea
JaretWright <hkkA223>
2017-06-08 09:54:48 -0400

    Initial commit

---
+++ Lesson7-June1-master/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $pageTitle ?></title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+</head>
+<body>
+
+<nav class="nav navbar-inverse">
+    <ul class="nav navbar-nav">
+        <li><a href="albums.php">Albums</a></li>
+        <li><a href="AlbumDetails.php">Album Details</a></li>
+        <li><a href="registration.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    </ul>
+</nav>
---
+++ Lesson7-June1-master/saveAlbum.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Saving Album...</title>
+</head>
+
+<body>
+<?php
+    $albumID = $_POST['albumID'];
+    $title = $_POST['title'];
+    $year = $_POST['year'];
+    $artist = $_POST['artist'];
+    $genre = $_POST['genre'];
+
+    //step 1 - connect to the database
+    require_once ('db.php');
+
+    //step 2 - create the SQL command to INSERT or UPDATE a record
+    if (!empty($albumID)){
+        $sql = "UPDATE albums  
+                   SET title = :title,
+                       year = :year,
+                       artist = :artist,
+                       genre = :genre
+                WHERE albumID = :albumID";}
+    else {
+        $sql = "INSERT INTO albums (title,   year,  artist,  genre) 
+                        VALUES (:title, :year, :artist, :genre);";
+    }
+
+
+    //step 3 - prepare the SQL command and bind the arguments to prevent SQL injection
+    $cmd = $conn->prepare($sql);
+    $cmd->bindParam(':title', $title, PDO::PARAM_STR, 50);
+    $cmd->bindParam(':year', $year, PDO::PARAM_INT, 4);
+    $cmd->bindParam(':artist', $artist, PDO::PARAM_STR, 50);
+    $cmd->bindParam(':genre', $genre, PDO::PARAM_STR, 20);
+
+    if (!empty($albumID))
+        $cmd->bindParam(':albumID', $albumID, PDO::PARAM_INT);
+
+    //step 4 - execute
+    $cmd->execute();
+
+    //step 5 - disconnect from database
+    $conn = null;
+
+    //step 6 - redirect to the albums page
+    header('location:albums.php');
+?>
+</body>
+
+</html>
